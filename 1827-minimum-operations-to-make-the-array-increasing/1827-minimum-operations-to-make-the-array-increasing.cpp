b952d16619f8be6fbdde50763cb01df1faec6375
Animesh Kumar <24animesh11@gmail.com>
2022-01-08 09:38:20 +0530

    Time: 28 ms (8.95%), Space: 15.5 MB (100.00%) - LeetHub

--- 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.cpp
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+    cin.tie(NULL); 
         int c = 0;
         for(int i = 1; i < nums.size(); i++) {
             if(nums[i] <= nums[i-1]) {
