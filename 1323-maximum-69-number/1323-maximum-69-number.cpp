ce48d1faf9495521a21a575b9f61b81ec23096a3
Animesh Kumar <24animesh11@gmail.com>
2022-01-08 09:25:00 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (20.21%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     int maximum69Number (int num) {
-        string s = to_string(num);
-        int i = 0;
-        while(i < s.size()) {
-            if(s[i] == '6') {
-                s[i] = '9';
-                break;
-            }
-            i++;
+        int lat = -1;
+        int m = num;
+        int p = 0;
+        while(m != 0) {
+            int val = m % 10;
+            if(val == 6)
+                lat = p;
+            m /= 10;
+            p++;
         }
-        return stoi(s);
+        if(lat == -1)
+            return num;
+        return num + 3*pow(10, lat);
     }
 };
