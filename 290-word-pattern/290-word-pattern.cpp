d6b71b51f31aa1ff7801ca16147819bd73872fae
Animesh Kumar <24animesh11@gmail.com>
2022-01-17 10:01:28 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (18.34%) - LeetHub

--- 290-word-pattern/290-word-pattern.cpp
+++ 290-word-pattern/290-word-pattern.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
     bool wordPattern(string pattern, string s) {
+        istringstream strcin(s);
+        string str;
+        vector<string> words;
+        while(strcin >> str) words.push_back(str);
+        if (pattern.size() != words.size()) 
+            return false;
         unordered_map<char, string> map1;
         unordered_map<string, char> map2;
-        int i = 0, j = 0;
-        for(; i < pattern.size() && j < s.size(); i++, j++) {
-            int st = j;
-            while(j < s.size() && s[j] != ' ') j++;
-            string word = s.substr(st, j - st);
+        for(int i = 0; i < pattern.size(); i++) {
             auto it1 = map1.find(pattern[i]);
-            auto it2 = map2.find(word);
+            auto it2 = map2.find(words[i]);
             if(it1 == map1.end() && it2 == map2.end()) {
-                map1[pattern[i]] = word;
-                map2[word] = pattern[i];
+                map1[pattern[i]] = words[i];
+                map2[words[i]] = pattern[i];
             } 
             else if(it1 != map1.end() && it2 != map2.end()) {
-                if(map1[pattern[i]] != word || map2[word] != pattern[i])
+                if(map1[pattern[i]] != words[i] || map2[words[i]] != pattern[i])
                     return false;
             }
             else
                 return false;
         }
-        return (i >= pattern.size() && j >= s.size());
+        return true;
     }
 };
