1503214f2479f790adcaeedc75abf3ad104d79e3
Animesh Kumar <24animesh11@gmail.com>
2022-01-19 18:03:50 +0530

    Time: 28 ms (44.01%), Space: 13.7 MB (65.90%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses, vector<int>());
+        for(vector<int> &p : prerequisites) 
+            adj[p[1]].push_back(p[0]);
+        vector<int> color(numCourses, 0);
+        // for(int i = 0; i < numCourses; i++) {
+        //     cout << i << ": ";
+        //     for(int x : adj[i])
+        //         cout << x << " ";
+        //     cout << endl;
+        // }
+        for(int i = 0; i < color.size(); i++) {
+            if(color[i] == 0) {
+                color[i] = 1;
+                if(has_cycle(i, adj, color))
+                    return false;
+                color[i] = 2;
+            }
+        }
+        return true;
+    }
+    
+    bool has_cycle(int u, const vector<vector<int>> &adj, vector<int> &color) {
+        for(int v : adj[u]) {
+            if(color[v] == 1)
+                return true;
+            if(color[v] == 2)
+                continue;
+            color[v] = 1;
+            if(has_cycle(v, adj, color))
+                return true;
+        }
+        color[u] = 2;
+        return false;
+    }
+};
