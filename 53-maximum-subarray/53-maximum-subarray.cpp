f7cae26dee8cd2099277d6e8db5978d2765c33a1
Animesh Kumar <24animesh11@gmail.com>
2022-02-02 11:58:36 +0530

    Time: 126 ms (54.37%), Space: 67.8 MB (53.83%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -3,12 +3,25 @@ public:
     int maxSubArray(vector<int>& nums) {
         int sum = nums[0];
         int i = 1;
+        int s = 0;
+        int S = s;
+        int E = i;
         int max_sum = sum;
         while(i < nums.size()) {
-            sum = sum < 0 ? nums[i] : sum + nums[i];
-            max_sum = max(max_sum, sum);
+            if(sum < 0) {
+                s = i;
+                sum = nums[i];
+            }
+            else
+                sum += nums[i];
+            if(sum > max_sum) {
+                S = s;
+                E = i;
+                max_sum = sum;
+            }
             i++;
         }
+        cout << S << ", " << E << endl;
         return max_sum;
         
     }
