5440ed5179148fb4bf7f6d0c7bd7e24de9d02d0c
Animesh Kumar <24animesh11@gmail.com>
2022-02-04 09:02:23 +0530

    Time: 24 ms (32.18%), Space: 13 MB (95.77%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -3,22 +3,37 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        vector<vector<bool>> visit(m, vector<bool>(n, false));
+        int var = 1;
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                if(visit[i][j] || matrix[i][j] != 0)
-                    continue;
-                for(int x = 0; x < m; x++) 
-                    visit[x][j] = matrix[x][j] != 0 ? true : false;
-                for(int y = 0; y < n; y++) 
-                    visit[i][y] = matrix[i][y] != 0 ? true : false;
+                if(matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    if(j == 0)
+                        var = 0;
+                    else 
+                        matrix[0][j] = 0;
+                }
             }
         }
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(visit[i][j])
+        for(int i = 1; i < m; i++) {
+            if(matrix[i][0] == 0) {
+                for(int j = 0; j < n; j++)
                     matrix[i][j] = 0;
             }
         }
+        int bef = matrix[0][0];
+        
+        for(int j = 0; j < n; j++) {
+            int temp = j==0 ? var : matrix[0][j];
+            if(temp == 0) {
+                for(int i = 0; i < m; i++)
+                    matrix[i][j] = 0;
+            }
+        }
+        if(bef == 0) {
+            for(int j = 0; j < n; j++)
+                matrix[0][j] = 0;
+        }
+        
     }
 };
