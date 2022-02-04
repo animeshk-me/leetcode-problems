d1221ee33d0ccdb6da96d431ee059d8668030af6
Animesh Kumar <24animesh11@gmail.com>
2022-02-04 09:28:22 +0530

    Time: 11 ms (6.46%), Space: 9.1 MB (29.29%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -3,19 +3,7 @@ public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         for(int i = 0; i < n; i++) 
             nums1[m+i] = nums2[i];
-        
-        int i = 0, j = m;
-        while(j < m+n && i < j) {
-            if(nums1[i] > nums1[j]) {
-                i_swap(j, i, nums1);
-                j++;
-            }
-            i++;
-        }
+        sort(nums1.begin(), nums1.end());    
     }
     
-    void i_swap(int j, int i, vector<int> &a) {
-        for(int k = j; k > i; k--)
-            swap(a[k], a[k-1]);
-    }
 };
