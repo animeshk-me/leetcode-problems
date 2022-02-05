61644081cfa485bff911293f878d19ac21731abd
Animesh Kumar <24animesh11@gmail.com>
2022-02-05 18:34:29 +0530

    Time: 86 ms (91.81%), Space: 8.4 MB (42.97%) - LeetHub

--- 2151-maximum-good-people-based-on-statements/2151-maximum-good-people-based-on-statements.cpp
+++ 2151-maximum-good-people-based-on-statements/2151-maximum-good-people-based-on-statements.cpp
@@ -2,31 +2,34 @@ class Solution {
 public:
     int maximumGood(vector<vector<int>>& statements) {
         int max_val = 0;
-        vector<bool> visit(statements.size(), false);
+        bitset<16> visit;
+        // vector<bool> visit(statements.size(), false);
         get_all_subsets(0, visit, max_val, statements);
         return max_val;
     }
     
-    void get_all_subsets(int i, vector<bool> visit, int &max_val, vector<vector<int>>& statements) {
+    void get_all_subsets(int i, bitset<16> visit, int &max_val, vector<vector<int>>& statements) {
         if(i == statements.size()) {
-            max_val = max(max_val, get_valid(visit, statements));
+            max_val = max(max_val, get_valid(visit, statements, max_val));
             return;
         }
         get_all_subsets(i+1, visit, max_val, statements);
-        visit[i] = true;
+        visit[i] = 1;
         get_all_subsets(i+1, visit, max_val, statements);
     }
     
-    int get_valid(vector<bool> &visit, vector<vector<int>>& statements) {
-        int count_friends = 0;
+    int get_valid(bitset<16> &visit, vector<vector<int>>& statements, int max_val) {
+        int count_friends = visit.count();
+        if(count_friends <= max_val)
+            return 0;
         int n = statements.size();
+
         for(int i = 0; i < n; i++) {
             if(visit[i]) {
                 for(int j = 0; j < n; j++) {
                     if(statements[i][j] != 2 && visit[j] != statements[i][j])
                         return 0;
                 }
-                count_friends++;
             }
         }
         return count_friends;
