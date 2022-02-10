7449811bfbc82e7ce511b599c8e8adbf8a75dcee
Animesh Kumar <24animesh11@gmail.com>
2022-02-10 08:55:07 +0530

    Time: 31 ms (36.63%), Space: 19.6 MB (88.74%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        map<int, int> hashmap;
-        for(int n : nums) {
-            auto it = hashmap.find(n);
-            if(it == hashmap.end())
-                hashmap[n] = 1;
+        sort(nums.begin(), nums.end());
+        int max_val = 1, max_id = 0;
+        int temp_val = 1;
+        for(int i = 1; i < nums.size(); i++) {
+            if(nums[i] == nums[i-1])
+                temp_val++;
             else
-                it->second++;
+                temp_val = 1;
+            if(temp_val > max_val) {
+                max_val = temp_val;
+                max_id = i;
+            }
+            if(max_val > nums.size() / 2)
+                return nums[max_id];
         }
-        for(auto &it : hashmap) {
-            if(it.second > (nums.size() / 2))
-                return it.first;
-        }
-        return 0;
+        return nums[max_id];
     }
 };
