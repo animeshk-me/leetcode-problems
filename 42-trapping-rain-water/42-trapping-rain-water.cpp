be5d58d4662028cd10ca33bc981edf58bfd82a56
Animesh Kumar <24animesh11@gmail.com>
2022-02-21 11:10:35 +0530

    Time: 15 ms (49.55%), Space: 18.4 MB (6.79%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,57 +1,22 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int left_sum = get_water_iterative(height);
-        reverse(height.begin(), height.end());
-        int right_sum = get_water_iterative(height);
-        return left_sum + right_sum;
-    }
-    
-    
-    int get_water_iterative(vector<int>& height) {
-        int area = 0;
-        int idx = 0;
         int n = height.size();
-        while(idx <= n - 2) {
-            int min_boundary_height = height[idx];
-            int i = idx + 1;
-            while(i < n && height[i] < min_boundary_height) 
-                i++;
-
-            if(i == n) {
-                idx = idx + 1;
-                continue;
-            }
-
-            for(int j = idx + 1; j < i; j++) {
-                area += min_boundary_height - height[j];
-                height[j] = min_boundary_height;
-            }
-            
-            idx = i;
-        }
-        return area;
+        vector<int> max_left(n, 0);
+        vector<int> max_right(n, 0);
+        
+        max_left[0] = height[0];
+        for(int i = 1; i < n; i++) 
+            max_left[i] = max(max_left[i-1], height[i]);
+        
+        max_right[n-1] = height[n-1];
+        for(int i = n - 2; i >= 0; i--)
+            max_right[i] = max(max_right[i+1], height[i]);
+        
+        int water_amount = 0;
+        for(int i = 0; i < n; i++)
+            water_amount += (min(max_left[i], max_right[i]) - height[i]);
+        
+        return water_amount;
     }
-    
-//     int get_water(int idx, vector<int>& height) {
-//         if(idx > (int)(height.size() - 3))
-//             return 0;
-//         // cout << "hello\n";
-//         int min_boundary_height = height[idx];
-//         int i = idx + 1;
-//         while(i < height.size() && height[i] < min_boundary_height) 
-//             i++;
-        
-//         if(i == height.size())
-//             return get_water(idx+1, height);
-        
-//         int temp_area = 0;
-//         for(int j = idx + 1; j < i; j++) {
-//             temp_area += min_boundary_height - height[j];
-//             height[j] = min_boundary_height;
-//         }
-        
-//         return temp_area + get_water(i, height);
-//     } 
-    // [23423,34534,34564,256,256,57235,4242,562,2422,4,24562,245,262,24244,2522]
 };
