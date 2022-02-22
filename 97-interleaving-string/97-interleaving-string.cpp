8e90c03a0da906a5da7b3d095b26ef847b9e47e0
Animesh Kumar <24animesh11@gmail.com>
2022-02-22 18:42:20 +0530

    Time: 3 ms (80.89%), Space: 7.1 MB (41.06%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -3,11 +3,6 @@ public:
     bool isInterleave(string s1, string s2, string s3) {
         if(s1.size() + s2.size() != s3.size())
             return false;
-        // vector<vector<int>> Memo(s1.size()+1, vector<int>(s2.size()+1, -1));
-        return is_interleaved_iterative(s1, s2, s3);
-    }
-    
-    bool is_interleaved_iterative(string& s1, string& s2, string& s3) {
         int m = s1.size(), n = s2.size();
         vector<vector<int>> Cache(m + 1, vector<int>(n + 1, 0));
         Cache[m][n] = 1;
@@ -22,23 +17,5 @@ public:
             }
         }
         return Cache[0][0];
-    }
-    
-    
-    bool is_interleaved(int i, int j, string& s1, string& s2, string& s3, vector<vector<int>> &Memo) {
-        int k = i + j;
-        if(k == s3.size())
-            return i == s1.size() && j == s2.size();
-        
-        if(Memo[i][j] == -1) {
-            Memo[i][j] = false;
-            if(i < s1.size() && s1[i] == s3[k])
-                Memo[i][j] |= is_interleaved(i+1, j, s1, s2, s3, Memo);
-            if(Memo[i][j] == 1)
-                return true;
-            if(j < s2.size() && s2[j] == s3[k])
-                Memo[i][j] |= is_interleaved(i, j+1, s1, s2, s3, Memo);
-        }
-        return Memo[i][j];
     }
 };
