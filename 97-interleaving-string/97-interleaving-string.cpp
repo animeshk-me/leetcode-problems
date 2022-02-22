fd554abe2bb6770fbc18d7ac75e21aa3a7afe2e4
Animesh Kumar <24animesh11@gmail.com>
2022-02-22 18:41:41 +0530

    Time: 4 ms (71.93%), Space: 7.1 MB (45.59%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -3,9 +3,27 @@ public:
     bool isInterleave(string s1, string s2, string s3) {
         if(s1.size() + s2.size() != s3.size())
             return false;
-        vector<vector<int>> Memo(s1.size()+1, vector<int>(s2.size()+1, -1));
-        return is_interleaved(0, 0, s1, s2, s3, Memo);
+        // vector<vector<int>> Memo(s1.size()+1, vector<int>(s2.size()+1, -1));
+        return is_interleaved_iterative(s1, s2, s3);
     }
+    
+    bool is_interleaved_iterative(string& s1, string& s2, string& s3) {
+        int m = s1.size(), n = s2.size();
+        vector<vector<int>> Cache(m + 1, vector<int>(n + 1, 0));
+        Cache[m][n] = 1;
+        for(int i = m; i >= 0; i--) {
+            for(int j = n; j >= 0; j--) {
+                if(i+j == m+n)
+                    continue;
+                if(i < m && s1[i] == s3[i+j])
+                    Cache[i][j] |= Cache[i+1][j];
+                if(j < n && s2[j] == s3[i+j])
+                    Cache[i][j] |= Cache[i][j+1];
+            }
+        }
+        return Cache[0][0];
+    }
+    
     
     bool is_interleaved(int i, int j, string& s1, string& s2, string& s3, vector<vector<int>> &Memo) {
         int k = i + j;
