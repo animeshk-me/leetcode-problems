8ea61992d7b23b8f8183050d7d9277c056fe60a4
Animesh Kumar <24animesh11@gmail.com>
2022-02-24 14:13:09 +0530

    Time: 239 ms (79.66%), Space: 13.8 MB (94.49%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,43 +1,5 @@
 class Solution {
 public:
-    vector<int> parent;
-    vector<int> rank;
-    
-    void make_set(int x) {
-        parent[x] = x;
-        rank[x] = 0;
-    }
-    
-    void union_set(int u, int v) {
-        link(find_set(u), find_set(v));
-    } 
-    
-    void link(int a, int b)  {
-        if(rank[a] > rank[b])
-            parent[b] = a;
-        else if(rank[a] < rank[b])
-            parent[a] = b;
-        else {
-            parent[b] = a;
-            rank[a]++;
-        }
-    }
-    
-    int find_set(int u) {
-        if(parent[u] != u)
-            parent[u] = find_set(parent[u]);
-        return parent[u];
-    }
-    
-    vector<pair<int,int>> get_edges(vector<vector<int>>& points) {
-        vector<pair<int,int>> edges;
-        for(int i = 0; i < points.size(); i++) {
-            for(int j = i+1; j < points.size(); j++)
-                edges.push_back({i, j});
-        }
-        return edges;
-    }
-
     
     int cost(pair<int,int> edge, vector<vector<int>>& points) {
         return abs(points[edge.first][0] - points[edge.second][0]) + abs(points[edge.first][1] - points[edge.second][1]);
@@ -45,25 +7,37 @@ public:
     
     int minCostConnectPoints(vector<vector<int>>& points) {
         int n = points.size();
-        int min_cost = 0;
-        parent = vector<int>(n, -1);
-        rank = vector<int>(n, 0);
-        for(int i = 0; i < n; i++)
-            make_set(i);
-        
-        vector<pair<int,int>> edges = get_edges(points);
-        // sort(edges.begin(), edges.end(), [](auto a, auto b) {return cost(a, points) < cost(b, points);});
-        sort(edges.begin(), edges.end(), [this, &points](pair<int,int> a, pair<int,int> b) {return cost(a, points) < cost(b, points);});
-        int i = 0;
-        for(auto edge : edges) {
-            if(i == n)
-                break;
-            int u = edge.first, v = edge.second;
-            if(find_set(u) != find_set(v)) {
-                union_set(u, v);
-                i++;
-                min_cost += cost(edge, points);
+        vector<bool> mst_set(n, false);
+        vector<int> parent(n, -1);
+        vector<int> key(n, INT_MAX);
+        key[0] = 0;
+        auto comp = [this, &points](auto& a, auto& b) {return a.second > b.second;};
+        priority_queue<pair<int,int>, vector<pair<int,int>>, decltype(comp)> pq(comp);
+        pq.push({0, key[0]});
+        while(!pq.empty()) {
+            auto temp = pq.top();
+            pq.pop();
+            int u = temp.first;
+            if(mst_set[u])
+                continue;
+            mst_set[u] = true;
+            for(int v = 0; v < n; v++) {
+                int weight = cost({u,v}, points);
+                if(!mst_set[v] && key[v] > weight) {
+                    key[v] = weight;
+                    parent[v] = u;
+                    pq.push({v, key[v]});
+                }
             }
+        }
+        return get_min_cost(parent, points);
+    }
+    
+    int get_min_cost(vector<int>& parent, vector<vector<int>>& points) {
+        int min_cost = 0;
+        for(int i = 0; i < parent.size(); i++) {
+            if(parent[i] != -1)
+                min_cost += cost({i, parent[i]}, points);
         }
         return min_cost;
     }
