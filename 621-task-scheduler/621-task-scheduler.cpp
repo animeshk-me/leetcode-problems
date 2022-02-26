64d3730f265dd62079f6efeefc67202e80845bc0
Animesh Kumar <24animesh11@gmail.com>
2022-02-26 16:42:50 +0530

    Time: 87 ms (71.89%), Space: 34.5 MB (69.63%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.cpp
+++ 621-task-scheduler/621-task-scheduler.cpp
@@ -2,37 +2,17 @@ class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
         vector<int> freq(26, 0);
-        vector<pair<int,int>> freq_sort;
-        vector<int> last_time(26, INT_MIN);
-        
         for(char task : tasks) 
             freq[task - 'A']++;
         
-        for(int i = 0; i < freq.size(); i++) {
-            if(freq[i] != 0)
-                freq_sort.push_back({i, freq[i]});
-        }
+        int max_freq = 0;
+        for(int f : freq)
+            max_freq = max(max_freq, f);
+    
+        int count = 0;    
+        for(int f : freq)
+            count = f == max_freq ? count + 1 : count;
         
-        int t = 0;
-        int total_count = 0;
-        while(total_count < tasks.size()) {
-            auto comp = [](auto &a, auto &b) {return a.second > b.second;};
-            sort(freq_sort.begin(), freq_sort.end(), comp);  
-            for(auto& el : freq_sort) {
-                if(el.second == 0)
-                    break;
-                if(last_time[el.first] + n < t) {
-                    // cout << "\n";
-                    el.second--;
-                    last_time[el.first] = t;
-                    total_count++;
-                    // cout << (char)(el.first+'A') << endl;
-                    break;
-                }
-            }
-            // cout << ": t = " << t << ", " << total_count << endl;
-            t++;
-        }
-        return t;
+        return max((int)tasks.size(), (max_freq-1) * n + max_freq + count - 1);
     }
 };
