9e4aefd3ac8bee386f3818b39c3f072cde5102ca
Animesh Kumar <24animesh11@gmail.com>
2022-03-03 15:28:27 +0530

    Time: 10 ms (66.25%), Space: 10.1 MB (58.28%) - LeetHub

---
+++ 139-word-break/139-word-break.cpp
@@ -0,0 +1,63 @@
+class Node {
+  public:
+    Node* children[26];
+    bool is_end;
+    Node() {
+        for(int i = 0; i < 26; i++)
+            children[i] = nullptr;
+        is_end = false;
+    }
+    
+    void insert(string s) {
+        Node* temp = this;
+        for(char c : s) {
+            if(temp->children[c-'a'] == nullptr)
+                temp->children[c-'a'] = new Node();
+            temp = temp->children[c-'a'];
+        }
+        temp->is_end = true;
+    }
+    
+    void print(string s) {
+        if(is_end)
+            cout << s << endl;
+        for(int i = 0; i < 26; i++) {
+            char p = i + 'a';
+            if(children[i] != nullptr)
+                children[i]->print(s + p);
+        }
+    }
+};
+
+
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        Node* trie = new Node();
+        vector<int> Memo(s.size(), -1);
+        for(auto& word : wordDict)
+            trie->insert(word);
+        // trie->print("");
+        return is_valid_break(0, s, trie, Memo);
+    }
+    
+    bool is_valid_break(int idx, string& s, Node* trie, vector<int>& Memo) {
+        if(idx >= s.size())
+            return true;
+
+        if(Memo[idx] == -1) {            
+            Node* temp = trie;
+            for(int i = idx; i <= s.size(); i++) {
+                if(temp->is_end && is_valid_break(i, s, trie, Memo))
+                    return true;
+                if(i == s.size())
+                    break;
+                if(temp == nullptr || temp->children[s[i]-'a'] == nullptr)
+                    break;
+                temp = temp->children[s[i]-'a'];
+            }
+            Memo[idx] = false;
+        }
+        return Memo[idx];
+    }
+};
