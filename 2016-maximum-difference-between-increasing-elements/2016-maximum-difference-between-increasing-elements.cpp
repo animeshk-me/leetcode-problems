f7f24d7d783442ad9c8da065d09039cbeeea6d00
Animesh Kumar <24animesh11@gmail.com>
2022-08-18 22:21:03 +0530

    Time: 11 ms (33.86%), Space: 8.4 MB (37.54%) - LeetHub

--- 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.cpp
+++ 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.cpp
@@ -4,8 +4,7 @@ public:
         int min_val = nums[0];
         int max_diff = -1;
         for(int i = 1; i < nums.size(); i++) {
-            if(min_val < nums[i]) 
-                max_diff = max(max_diff, nums[i] - min_val);
+            max_diff = max(max_diff, nums[i] - min_val);
             min_val = min(min_val, nums[i]);
         }
         return max_diff <= 0 ? -1 : max_diff;
