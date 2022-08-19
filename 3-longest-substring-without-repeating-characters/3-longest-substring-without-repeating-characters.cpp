1e4f243ee6d9e538a8acc46be49c750787be351e
Animesh Kumar <24animesh11@gmail.com>
2022-08-19 16:40:32 +0530

    Time: 12 ms (81.94%), Space: 7.1 MB (88.98%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -4,23 +4,17 @@ public:
         int i = 0;
         int j = i;
         int max_len = 0;
-        vector<bool> visit(195, false);
+        vector<bool> visit(126, false);
         while(j < s.size()) {
-            while(j < s.size() && !visit[s[j] - ' ']) {
-                // cout << "hi: " << s[j] - ' ' << endl;
+            while(j < s.size() && !visit[s[j] - ' ']) 
                 visit[s[j++] - ' '] = true;
-            }
             max_len = max(max_len, j - i);
             if(j >= s.size())
                 break;
-            while(s[i] != s[j]) { 
-                // cout << "hi2: " <<  s[i] - ' ' << endl;
+            while(s[i] != s[j]) 
                 visit[s[i++] - ' '] = false;
-            }
-            // cout << "hi3: " << s[i] << ": " << i << ": " << j << ": "<< s[i] - ' ' << endl;
             visit[s[i++] - ' '] = false;
         }
         return max_len;
     }
-// "abc;azq^%&*+(/*"
 };
