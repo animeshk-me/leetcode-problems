91ae29aa81cdf1e406c13d00b402456491c34fc3
Animesh Kumar <24animesh11@gmail.com>
2022-08-19 12:38:08 +0530

    Time: 116 ms (76.28%), Space: 41.5 MB (77.97%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-    unordered_map<int,int> M;
-    int pre_sum = 0;
-    int count = 0;
-    M.insert({pre_sum, 1});
-    
-    for(auto& x : nums) {
-				// dynamically calculate pre_sum
-        pre_sum += x;
-
-				// check if a pre_sum2 exists
-        auto it = M.find(pre_sum - k);
-        if(it != M.end())
-            count += it->second;
-
-        // increment the count of pre_sum in the map M
-				it = M.find(pre_sum);
-        if(it != M.end())
-            it->second++;
-        else
-            M.insert({pre_sum, 1});
+        unordered_map<int,int> M;
+        int pre_sum = 0;
+        int count = 0;
+        M.insert({pre_sum, 1});
+        
+        for(auto& x : nums) {
+            pre_sum += x;
+            auto it = M.find(pre_sum - k);
+            if(it != M.end())
+                count += it->second;
+            it = M.find(pre_sum);
+            if(it != M.end())
+                it->second++;
+            else
+                M.insert({pre_sum, 1});
+        }
+        return count;
     }
-    return count;
-}
 };
